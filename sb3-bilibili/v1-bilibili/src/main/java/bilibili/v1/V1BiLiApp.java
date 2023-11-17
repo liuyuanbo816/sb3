ab0c1d1492b33945ea0c167f355a9abaa81cc6db
Zmz <Zmz@gmail.com>
2023-11-17 22:29:12 +0800

    添加 v1-bilibili 项目初始化环境

---
+++ sb3-bilibili/v1-bilibili/src/main/java/bilibili/v1/V1BiLiApp.java
@@ -0,0 +1,11 @@
+package bilibili.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V1BiLiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(V1BiLiApp.class, args);
+    }
+}

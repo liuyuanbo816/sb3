58f9e87a94492fcbc834f08d8feed55aa71476ce
Zmz <Zmz@gmail.com>
2024-01-13 17:17:57 +0800

    add feishu test

---
+++ sb3-new-features/src/main/java/org/zmz/sb3/newfeatures/algorithm/feishu/AssertSaveUser.java
@@ -0,0 +1,8 @@
+package org.zmz.sb3.newfeatures.algorithm.feishu;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AssertSaveUser {
+
+}
--- sb3-new-features/src/test/java/org/zmz/sb3/newfeatures/test/switches/NewSwitchTest.java
+++ sb3-new-features/src/test/java/org/zmz/sb3/newfeatures/test/switches/NewSwitchTest.java
@@ -2,6 +2,10 @@ package org.zmz.sb3.newfeatures.test.switches;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class NewSwitchTest {
     @Test
     public void test1() {
@@ -23,5 +27,27 @@ public class NewSwitchTest {
             default -> throw new IllegalArgumentException("非法时间");
         };
         System.out.println(s);
+    }
+
+    @Test
+    public void test2() {
+        List<String> lst1 = List.of("1", "2", "3");
+        List<String> lst2 = List.of("1", "2", "3");
+
+        System.out.println(Objects.equals(lst1, lst2));
+    }
+
+    @Test
+    public void test3() {
+        List<Integer> lst1 = new ArrayList<>();
+        lst1.add(1);
+        lst1.add(2);
+        lst1.add(3);
+        List<Integer> lst2 = new ArrayList<>();
+        lst2.add(1);
+        lst2.add(2);
+        lst2.add(3);
+
+        System.out.println(Objects.equals(lst1, lst2));
     }
 }

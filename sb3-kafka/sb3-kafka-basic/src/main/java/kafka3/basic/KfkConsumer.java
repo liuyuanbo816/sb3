034c35e58a081504aeea7721b4821f6153441049
Zmz <zjcspringboot@gmail.com>
2024-04-06 02:16:56 +0800

    演示 kafka 生产者 消费者 demo

--- sb3-kafka/sb3-kafka-basic/src/main/java/kafka3/basic/KfkConsumer.java
+++ sb3-kafka/sb3-kafka-basic/src/main/java/kafka3/basic/KfkConsumer.java
@@ -11,20 +11,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class KfkConsumer {
+
+    public static final String TOPIC_NAME = "mytopic";
+
     public static void main(String[] args) {
         // 创建配置对象
         Map<String, Object> configMap = new HashMap<>(8);
-        configMap.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.110.211:9092");
+        configMap.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.110.201:9092");
         configMap.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         configMap.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         configMap.put(ConsumerConfig.GROUP_ID_CONFIG, "zmz816");
 
-        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(configMap);
-        kafkaConsumer.subscribe(Collections.singletonList("test2"));
-        for (; ; ) {
-            ConsumerRecords<String, String> datas = kafkaConsumer.poll(Duration.ofSeconds(10));
-            datas.forEach(System.out::println);
+        try (KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(configMap)) {
+            kafkaConsumer.subscribe(Collections.singletonList(TOPIC_NAME));
+            for (; ; ) {
+                ConsumerRecords<String, String> datas = kafkaConsumer.poll(Duration.ofSeconds(10));
+                datas.forEach(System.out::println);
+            }
         }
-
     }
 }
--- sb3-kafka/sb3-kafka-basic/src/main/java/kafka3/basic/KfkProducer.java
+++ sb3-kafka/sb3-kafka-basic/src/main/java/kafka3/basic/KfkProducer.java
@@ -9,16 +9,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class KfkProducer {
+
+    public static final String TOPIC_NAME = "mytopic";
+
     public static void main(String[] args) {
         // 创建配置对象
         Map<String, Object> configMap = new HashMap<>(8);
-        configMap.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.110.211:9092");
+        configMap.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.110.201:9092");
         configMap.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         configMap.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
         KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(configMap);
         for (int i = 1; i <= 10; i++) {
-            ProducerRecord<String, String> producerRecord = new ProducerRecord<>("test2", "k" + i, "v" + i);
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<>(TOPIC_NAME, "key-" + i, "value-" + i);
             // 创建 Kafka 生产者 将数据发送到 Broker
             kafkaProducer.send(producerRecord);
         }

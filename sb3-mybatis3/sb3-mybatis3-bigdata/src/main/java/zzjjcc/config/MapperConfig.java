62e444207fceb977f4eb65949f7d1732ac36d3e1
zzjjcc <zzjjcc@yoga14s.com>
2024-05-09 22:59:20 +0800

    fix 多数据源 驼峰失效问题

--- sb3-mybatis3/sb3-mybatis3-bigdata/src/main/java/zzjjcc/config/MapperConfig.java
+++ sb3-mybatis3/sb3-mybatis3-bigdata/src/main/java/zzjjcc/config/MapperConfig.java
@@ -6,6 +6,7 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -22,11 +23,18 @@ public class MapperConfig {
     @Resource
     private DataSource dataSource;
 
+    @Bean
+    @ConfigurationProperties(prefix = "mybatis.configuration")
+    public org.apache.ibatis.session.Configuration globalMybatisConfiguration() {
+        return new org.apache.ibatis.session.Configuration();
+    }
+
     @Bean(name = "sqlSessionFactory")
     @Primary
     public SqlSessionFactory sqlSessionFactory() throws Exception {
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
         sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setConfiguration(globalMybatisConfiguration());
         sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver()
                 .getResources("classpath:mapper/*Mapper.xml"));
         return sqlSessionFactoryBean.getObject();

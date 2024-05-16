1822ff93328f0bb3ac046e9249add810a2ec80f5
zjc@0027028800 <zheng.jianchao@iwhalecloud.com>
2024-05-16 19:42:53 +0800

    基础 poi 2

--- sb3-kafka/sb3-kafka-basic/src/main/java/kafka3/service/impl/PoiServiceImpl.java
+++ sb3-kafka/sb3-kafka-basic/src/main/java/kafka3/service/impl/PoiServiceImpl.java
@@ -3,7 +3,15 @@ package kafka3.service.impl;
 import kafka3.common.ExportExcelUtil;
 import kafka3.common.ImportExcelUtil;
 import kafka3.service.PoiService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.VerticalAlignment;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
@@ -12,7 +20,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -20,6 +27,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class PoiServiceImpl implements PoiService {
     @Override
     public String importExcel(File file) {
@@ -41,10 +49,9 @@ public class PoiServiceImpl implements PoiService {
             }
             //拿到数据进行数据库操作
             System.out.println(headData);
-            SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
             List<String[]> list = Optional.ofNullable(dataList).orElse(new ArrayList<>());
             for (String[] strArr : list) {
-                Map<String, ?> entityMap = new LinkedHashMap<>();
+                Map<String, String> entityMap = new LinkedHashMap<>();
                 for (int i = 0; i < strArr.length; i++) {
                     String cellValue = strArr[i];
                     switch (i) {
@@ -55,13 +62,13 @@ public class PoiServiceImpl implements PoiService {
                             entityMap.put("sex", cellValue);
                             break;
                         case 2:
-                            entityMap.put("age", Integer.valueOf(cellValue));
+                            entityMap.put("age", cellValue);
                             break;
                         case 3:
-                            entityMap.put("height", Double.valueOf(cellValue));
+                            entityMap.put("height", cellValue);
                             break;
                         case 4:
-                            entityMap.put("birth", sdf.parse(cellValue));
+                            entityMap.put("birth", cellValue);
                             break;
                         case 5:
                             entityMap.put("intro", cellValue);
@@ -73,13 +80,13 @@ public class PoiServiceImpl implements PoiService {
             }
             return "success";
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("异常01", e);
         } finally {
             if (workbook != null) {
                 try {
                     workbook.close();
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    log.error("异常02", e);
                 }
             }
         }
@@ -88,10 +95,10 @@ public class PoiServiceImpl implements PoiService {
 
     public byte[] exportExcel(String fileName) {
         ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
-        List<Map<String, Object>> exportDataList = poiEntityDOMapper.selectByExport();
+        List<Map<String, Object>> exportDataList = mock();
         String head = "2020-3-20人员信息登记表";
         String[] fieldNameCNArr = new String[]{"姓名", "性别", "年龄", "身高（m）", "出生日期", "创建时间", "简介"};
-        String[] fieldNameENArr = new String[]{"name", "sex", "age", "height", "birthday", "createtime", "intro"};
+        String[] fieldNameENArr = new String[]{"name", "sex", "age", "height", "birthday", "createTime", "intro"};
 
         Workbook workbook = null;
         try {
@@ -100,59 +107,64 @@ public class PoiServiceImpl implements PoiService {
             } else if (ImportExcelUtil.isExcel2003(fileName)) {
                 workbook = new HSSFWorkbook();
             }
+            Sheet sheet;
             //创建一张excel表
-            Sheet sheet = workbook.createSheet("sheet1");
-            // 初始化列的宽度
-            for (int i = 0; i < fieldNameCNArr.length; i++) {
-                switch (i) {
-                    case 4:
-                        ExportExcelUtil.createColumnWidth(sheet, i, 20);
-                        break;
-                    case 5:
-                        ExportExcelUtil.createColumnWidth(sheet, i, 20);
-                        break;
-                    case 6:
-                        ExportExcelUtil.createColumnWidth(sheet, i, 30);
-                        break;
-                    default:
-                        ExportExcelUtil.createColumnWidth(sheet, i, 10);
-                        break;
+            if (workbook != null) {
+                sheet = workbook.createSheet("sheet1");
+                // 初始化列的宽度
+                for (int i = 0; i < fieldNameCNArr.length; i++) {
+                    switch (i) {
+                        case 4, 5:
+                            ExportExcelUtil.createColumnWidth(sheet, i, 20);
+                            break;
+                        case 6:
+                            ExportExcelUtil.createColumnWidth(sheet, i, 30);
+                            break;
+                        default:
+                            ExportExcelUtil.createColumnWidth(sheet, i, 10);
+                            break;
+                    }
                 }
-            }
+                //创建并初始化表头
+                int headStartRow = 0;
+                CellStyle style = ExportExcelUtil.createCellStyle(workbook, "宋体", true, (short) 18, Font.COLOR_NORMAL);
+                ExportExcelUtil.setCellStyle(style, HorizontalAlignment.CENTER, VerticalAlignment.CENTER, false);
+                ExportExcelUtil.creatSheetHead(sheet, style, head, headStartRow, 0, 0, 0, 6);
+                //创建并初始化字段行
+                int feildStartRow = 1;
+                Row fieldRow = sheet.createRow(feildStartRow);
+                style = ExportExcelUtil.createCellStyle(workbook, "宋体", true, (short) 14, Font.COLOR_NORMAL);
+                ExportExcelUtil.setCellStyle(style, HorizontalAlignment.CENTER, VerticalAlignment.CENTER, true);
+                for (int i = 0; i < fieldNameCNArr.length; i++) {
+                    Cell cell = fieldRow.createCell(i);
+                    cell.setCellStyle(style);
+                    cell.setCellValue(fieldNameCNArr[i]);
+                }
 
-            //创建并初始化表头
-            int headStartRow = 0;
-            CellStyle style = ExportExcelUtil.createCellStyle(workbook, "宋体", true, (short) 18, Font.COLOR_NORMAL);
-            ExportExcelUtil.setCellStyle(style, HorizontalAlignment.CENTER, VerticalAlignment.CENTER, false);
-            ExportExcelUtil.creatSheetHead(sheet, style, head, headStartRow, 0, 0, 0, 6);
-            //创建并初始化字段行
-            int feildStartRow = 1;
-            Row fieldRow = sheet.createRow(feildStartRow);
-            style = ExportExcelUtil.createCellStyle(workbook, "宋体", true, (short) 14, Font.COLOR_NORMAL);
-            ExportExcelUtil.setCellStyle(style, HorizontalAlignment.CENTER, VerticalAlignment.CENTER, true);
-            for (int i = 0; i < fieldNameCNArr.length; i++) {
-                Cell cell = fieldRow.createCell(i);
-                cell.setCellStyle(style);
-                cell.setCellValue(fieldNameCNArr[i]);
+                //从数据开始行创建并初始化数据单元格
+                int dataStartRow = 2;
+                style = ExportExcelUtil.createCellStyle(workbook, "宋体", false, (short) 12, Font.COLOR_NORMAL);
+                ExportExcelUtil.setCellStyle(style, HorizontalAlignment.CENTER, VerticalAlignment.CENTER, true);
+                ExportExcelUtil.fillSheetData(sheet, style, dataStartRow, exportDataList, fieldNameENArr);
+                workbook.write(byteOut);
             }
-
-            //从数据开始行创建并初始化数据单元格
-            int dataStartRow = 2;
-            style = ExportExcelUtil.createCellStyle(workbook, "宋体", false, (short) 12, Font.COLOR_NORMAL);
-            ExportExcelUtil.setCellStyle(style, HorizontalAlignment.CENTER, VerticalAlignment.CENTER, true);
-            ExportExcelUtil.fillSheetData(sheet, style, dataStartRow, exportDataList, fieldNameENArr);
-            workbook.write(byteOut);
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error("异常03", e);
         } finally {
             if (workbook != null) {
                 try {
                     workbook.close();
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    log.error("异常04", e);
                 }
             }
         }
         return byteOut.toByteArray();
+    }
+
+    private List<Map<String, Object>> mock() {
+        return List.of(
+                Map.of()
+        );
     }
 }

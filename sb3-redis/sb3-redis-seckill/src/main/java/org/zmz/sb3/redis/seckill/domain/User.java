6f926db19c2ba25a8b234f9848961ea3fdd736cf
zzjjcc <zzjjcc@163.com>
2024-10-11 22:38:34 +0800

    添加 mapper && domain

---
+++ sb3-redis/sb3-redis-seckill/src/main/java/org/zmz/sb3/redis/seckill/domain/User.java
@@ -0,0 +1,54 @@
+package org.zmz.sb3.redis.seckill.domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.*;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@TableName("tb_user")
+public class User implements Serializable {
+
+    /**
+     * 主键
+     */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 手机号码
+     */
+    private String phone;
+
+    /**
+     * 密码，加密存储
+     */
+    private String password;
+
+    /**
+     * 昵称，默认是随机字符
+     */
+    private String nickName;
+
+    /**
+     * 用户头像
+     */
+    private String icon;
+
+    /**
+     * 创建时间
+     */
+    private LocalDateTime createTime;
+
+    /**
+     * 更新时间
+     */
+    private LocalDateTime updateTime;
+
+
+}
---
+++ sb3-redis/sb3-redis-seckill/src/main/java/org/zmz/sb3/redis/seckill/mapper/BlogMapper.java
@@ -0,0 +1,8 @@
+package org.zmz.sb3.redis.seckill.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.zmz.sb3.redis.seckill.domain.Blog;
+
+public interface BlogMapper extends BaseMapper<Blog> {
+
+}

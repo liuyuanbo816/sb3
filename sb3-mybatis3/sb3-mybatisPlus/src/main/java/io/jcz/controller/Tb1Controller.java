2caaa17abf8955b17723eb9f0ded38e41c290309
zzjjcc <zheng.jianchao@iwhalecloud.com>
2024-11-25 21:21:51 +0800

    完成多数据源测试 4

--- sb3-mybatis3/sb3-mybatisPlus/src/main/java/io/jcz/controller/Tb1Controller.java
+++ sb3-mybatis3/sb3-mybatisPlus/src/main/java/io/jcz/controller/Tb1Controller.java
@@ -1,6 +1,7 @@
 package io.jcz.controller;
 
 import io.jcz.common.R;
+import io.jcz.mapper.Tb1Mapper;
 import io.jcz.model.Tb1;
 import io.jcz.service.Tb1Service;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -17,9 +19,26 @@ public class Tb1Controller {
     @Autowired
     private Tb1Service tb1Service;
 
+    @Autowired
+    Tb1Mapper tb1Mapper;
+
     @GetMapping("/list")
     public R<List<Tb1>> list() {
         return R.ok(tb1Service.findAll());
+    }
+
+    @GetMapping("/insert")
+    public R<String> insert() {
+        Tb1 tb1 = new Tb1();
+        tb1.setName("haha");
+        tb1.setBirth(new Date());
+        tb1Mapper.insert(tb1);
+
+        Tb1 tb2 = new Tb1();
+        tb2.setName("gege");
+        tb2.setBirth(new Date());
+        tb1Mapper.insert(tb2);
+        return R.ok("插入成功");
     }
 
 }

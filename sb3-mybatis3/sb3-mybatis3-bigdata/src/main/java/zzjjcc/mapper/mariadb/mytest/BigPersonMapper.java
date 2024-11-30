0fa3322327e0be68ec4e29bf4d27db4b8253e6ca
zzjjcc <zheng.jianchao@iwhalecloud.com>
2024-12-01 00:09:07 +0800

    测试 1

--- sb3-mybatis3/sb3-mybatis3-bigdata/src/main/java/zzjjcc/mapper/mariadb/mytest/BigPersonMapper.java
+++ sb3-mybatis3/sb3-mybatis3-bigdata/src/main/java/zzjjcc/mapper/mariadb/mytest/BigPersonMapper.java
@@ -1,4 +1,10 @@
 package zzjjcc.mapper.mariadb.mytest;
 
+import zzjjcc.model.mariadb.mytest.BigPerson;
+
+import java.util.List;
+import java.util.Map;
+
 public interface BigPersonMapper {
+    List<BigPerson> lst1(Map<String, Object> param);
 }

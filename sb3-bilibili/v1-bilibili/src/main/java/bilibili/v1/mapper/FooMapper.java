4cdd6c919b39029d3bf677ed20b717239f129049
zzjjcc <zheng.jianchao@iwhalecloud.com>
2024-12-01 22:27:06 +0800

    移除一些非必要模块

--- sb3-bilibili/v1-bilibili/src/main/java/bilibili/v1/mapper/FooMapper.java
+++
@@ -1,7 +0,0 @@
-package bilibili.v1.mapper;
-
-import java.time.LocalDateTime;
-
-public interface FooMapper {
-    LocalDateTime getDBNow();
-}
--- sb3-vertx-counter/src/main/resources/db/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO tb_account (id, name, balance)
-VALUES (1, 'yuan', 800);
-INSERT INTO tb_account (id, name, balance)
-VALUES (2, 'zhi', 600);
-INSERT INTO tb_account (id, name, balance)
-VALUES (3, 'yang', 750);

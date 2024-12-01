3cd6113e6e0762c77482c4d41ccd99939dbb0fe9
zzjjcc <zheng.jianchao@iwhalecloud.com>
2024-12-01 23:02:13 +0800

    降级版本为 3.3.6 增加 gateway 模块

---
+++ sb3-microcloud/gateway/src/main/java/zzjjcc/gateway/GatewayServerApp.java
@@ -0,0 +1,11 @@
+package zzjjcc.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServerApp.class, args);
+    }
+}

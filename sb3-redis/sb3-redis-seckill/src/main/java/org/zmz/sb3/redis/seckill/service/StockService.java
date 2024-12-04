29398da7db8d489741b515608802ebefebf701e3
zzjjcc <zheng.jianchao@iwhalecloud.com>
2024-12-05 07:53:51 +0800

    添加分布式锁demo

--- sb3-redis/sb3-redis-seckill/src/main/java/org/zmz/sb3/redis/seckill/service/StockService.java
+++ sb3-redis/sb3-redis-seckill/src/main/java/org/zmz/sb3/redis/seckill/service/StockService.java
@@ -4,16 +4,26 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.zmz.sb3.redis.seckill.domain.Stock;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 @Service
 @Slf4j
 public class StockService {
 
     Stock stock = new Stock(5000);
 
+    ReentrantLock lock = new ReentrantLock();
+
     public void deduce() {
-        int count = stock.getCount();
-        count--;
-        stock.setCount(count);
-        log.info("剩余库存: {}", stock.getCount());
+        lock.lock();
+
+        try {
+            int count = stock.getCount();
+            count--;
+            stock.setCount(count);
+            log.info("剩余库存: {}", stock.getCount());
+        } finally {
+            lock.unlock();
+        }
     }
 }

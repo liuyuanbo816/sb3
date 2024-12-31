cc92a8fc413cf9e84d22a9606bc6c59db334195d
zzjjcc <zheng.jianchao@iwhalecloud.com>
2024-12-31 15:03:34 +0800

    添加测试模块

---
+++ sb3-test/spring-boot3-test/src/main/java/io/jcz/SpringBoot3TestApp.java
@@ -0,0 +1,11 @@
+package io.jcz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3TestApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3TestApp.class, args);
+    }
+}
